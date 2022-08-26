80bd0fe3b7eee9c536fb7704665f0deba8598f2c
Thiago Novato <thiagonovato@gmail.com>
2022-08-26 08:44:42 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const randomWidth = useSharedValue(300);
+  const rounded = useSharedValue(0);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+      borderRadius: withTiming(rounded.value, config),
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Animated.View
+        style={[
+          {
+            width: 100,
+            height: 300,
+            backgroundColor: 'black',
+            margin: 30,
+            borderRadius: 50,
+          },
+          style,
+        ]}
+      />
+      <Button
+        title='Mude!'
+        onPress={() => {
+          randomWidth.value = Math.random() * 300;
+          rounded.value = Math.random() * 300;
+        }}
+      />
     </View>
   );
 }
@@ -15,6 +54,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+## Project using React Native Reanimated
+
+#### Links
+
+I used Expo + React Native Reanimated => [Link](https://docs.expo.dev/versions/latest/sdk/reanimated/).
+
+### Running the project
+
+Use `yarn` to install dependencies and then `yarn start` to run.
+
+##### by thiagonovato
